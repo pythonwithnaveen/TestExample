41ed293d737c9abae62f899a744f4b927d831d49
naveen <naveen007@gmail.com>
2021-02-15 06:06:58 +0000

    a

---
+++ Demo8.py
@@ -0,0 +1,18 @@
+def show():
+    a = 100
+    b = 200
+    print(a+b)
+
+
+def display():
+    a = 500
+    b = 1000
+    print(a+b)
+
+print("Hi")
+show()
+print("------")
+display()
+print("------")
+show()
+print("Bye")
